4b16d7f47116078bc46f11a004681fda853a6b73
jschang19 <wwwsean13579@gmail.com>
2024-05-11 13:05:54 +0800

    chore: translate login form

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -95,7 +95,7 @@ function LoginButton() {
       className="my-4 flex h-10 w-full flex-row items-center justify-center rounded-md bg-zinc-900 p-2 text-sm font-semibold text-zinc-100 hover:bg-zinc-800 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-200"
       aria-disabled={pending}
     >
-      {pending ? <IconSpinner /> : 'Log in'}
+      {pending ? <IconSpinner /> : '登入'}
     </button>
   )
 }
